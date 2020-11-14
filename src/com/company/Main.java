c2418c6b9293cd444c0f48824dc21b6ccad083c8
Sayu98 <74456630+Sayu98@users.noreply.github.com>
2020-11-14 15:41:00 +0100

    Veeel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("geld");
         System.out.println("hah welloe");
         System.out.println("Laten we stoppen man, ik spreekje morgen!");
+        System.out.println("Veel geld");
     }
 }
